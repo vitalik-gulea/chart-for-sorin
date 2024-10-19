2eb5544c5912a5f719d10e561a63963cfa0fe0d2
gulea <vitalii.g@gbmf.tech>
2024-10-19 17:51:32 +0300

    fix bug for deploy and add new lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  DatePicker,
+  DateValue,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+import Chart from "./Chart/Chart";
+import { useState } from "react";
+import { getLocalTimeZone, now } from "@internationalized/date";
+import { IChartLines } from "./Chart/ChartLines";
+import { getEventValue, getFormattedTimeFromTimestamp } from "./untils/utils";
+const App = () => {
+  const [date, setDate] = useState<DateValue>(now(getLocalTimeZone()));
+  const [data] = useState<IChartLines[]>([
+    {
+      x: 1729285200 * 1000,
+      y: 1,
+      color: "bg-red-400",
+    },
+    {
+      x: 1729288800 * 1000,
+      y: 3,
+      color: "bg-blue-400",
+    },
+    {
+      x: 1729292400 * 1000,
+      y: 4,
+      color: "bg-green-400",
+    },
+    {
+      x: 1729337400 * 1000,
+      y: 2,
+      color: "bg-yellow-400",
+    },
+  ]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="w-screen h-screen flex justify-center  items-center">
+      <Card className="w-[90%] h-[75%]">
+        <CardHeader className="gap-[15px]">
+          Chart
+          <DatePicker
+            label="Birth date"
+            defaultValue={now(getLocalTimeZone())}
+            maxValue={now(getLocalTimeZone())}
+            value={date}
+            onChange={(e) => setDate(e)}
+            granularity="day"
+            className="max-w-[284px]"
+          />
+        </CardHeader>
+        <CardBody className="gap-[50px]">
+          <Chart data={data} columns={24} rows={4} date={date} />
+          <Table>
+            <TableHeader>
+              <TableColumn>Date</TableColumn>
+              <TableColumn>Event</TableColumn>
+            </TableHeader>
+            <TableBody>
+              {data.map((value, index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    {getFormattedTimeFromTimestamp(value.x)}
+                  </TableCell>
+                  <TableCell>{getEventValue(value.y)}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+
+createRoot(document.getElementById("root")!).render(
+    <NextUIProvider>
+      <App/>
+    </NextUIProvider>
+);
